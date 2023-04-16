2400991b80e4ad905b971bbd69f1991911f3cc2c
AntonZhiganov <Antohajigan@gmail.com>
2023-04-16 22:53:59 +0200

    Completed task 4, lectures 3

---
+++ Lection_3/Task_4/Task_4.py
@@ -0,0 +1,25 @@
+import requests
+
+def mailServer (urls):
+    results = []
+    
+    for url in urls:
+        response = requests.get(url)
+        
+        if response.status_code in [200, 302]:
+            results.append("OK")
+        else:
+            results.append("FAIL")
+    return results
+
+urls = [ "https://2ch.hk" ]
+
+results = mailServer(urls)
+
+print(results)
+
+    
+    
+    
+    
+    
