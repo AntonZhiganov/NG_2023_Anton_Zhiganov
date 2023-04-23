1a316e32a2d6361661dc3c680923550e61b69a1b
AntonZhiganov <Antohajigan@gmail.com>
2023-04-23 16:08:55 +0200

    Corrected errors in task 3 of lecture 1

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -1,37 +1,40 @@
 import math
 
-x = int  (input("Enter x: "))
-y = int  (input("Enter y: "))
+firstNum = int  (input("Enter first number : "))       #Asking the user to enter 2 numbers
+secondNum = int  (input("Enter second number: "))
 
-print("1 - (+)")
+print("1 - (+)")                #let the user choose an action
 print("2 - (-)")
 print("3 - (*)")
 print("4 - (:)")
 print("5 - (exponentiation)")
 print("6 - (Square root)")
 
-z = int (input("What do you want to do with numbers: " ))
+action = int (input("What do you want to do with numbers: " ))
 
-print("===========================================")
+print("===========================================")  
 
-if z == 1 :
-    print("result of addition: " + (str(x + y)))
-if z == 2 :
-    print("subtraction result: " + (str(x - y)))
-if z == 3 :
-    print("result of multiplication: " + (str(x * y)))
-if z == 4 :
-    print("result of division: " + (str(x / y)))
-if z == 5 :
-    print("result of exponentiation: " + (str(x ** y)))
-if z == 6 :
+if action == 1 :                                          #The program reads the user's choice, 
+    print("result of addition: " + (str(firstNum + secondNum)))          #If the user enters a different number, then we inform him about it.
+elif action == 2 :
+    print("subtraction result: " + (str(firstNum - secondNum)))
+elif action == 3 :
+    print("result of multiplication: " + (str(firstNum * secondNum)))
+elif action == 4 :
+    print("result of division: " + (str(firstNum / secondNum)))
+elif action == 5 :
+    print("result of exponentiation: " + (str(firstNum ** secondNum)))
+elif action == 6 :
     print("What number do you want to extract the square root of? ")
-    print("Enter 1 if you want to take the square root of x ")
-    print("Enter 2 if you want to take the square root of y ")
+    print("Enter 1 if you want to take the square root of first number ")
+    print("Enter 2 if you want to take the square root of second number ")
     
-d = int (input("Do you want to take the square root of x or y?: "))
+    choice = int (input("Do you want to take the square root of first number or secons number?: "))
 
-if d == 1 :
-    print("The result of extracting the root from the number" + (str(x)) + "is: " + (str(x ** (0.5))))
-if d == 2 :
-    print("The result of extracting the root from the number" + (str(y)) + "is: " + (str(y ** (0.5))))
+    if choice == 1 :
+        print("The result of extracting the root from the number" + (str(firstNum)) + "is: " + (str(firstNum ** (0.5))))
+    if choice == 2 :
+        print("The result of extracting the root from the number" + (str(secondNum)) + "is: " + (str(secondNum ** (0.5))))
+        
+else :
+    print ("Enter numbers from 1 to 6!")
