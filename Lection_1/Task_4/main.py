dd8afb69482dc7cfb6bc81962afb01d851475d30
AntonZhiganov <Antohajigan@gmail.com>
2023-04-25 15:15:57 +0200

    Edited the code for 4 assignments 1 lecture

--- Lection_1/Task_4/main.py
+++ Lection_1/Task_4/main.py
@@ -3,20 +3,17 @@ print("a - First element, b - second element, c - third element")
 a = float (input("enter a (a can't be 0): "))
 b = float (input("enter b : "))
 c = float (input("enter c : "))
-d = float
-x1 = float
-x2 = float
 
 if a == 0 :
     print(" Error - a can't be 0")
 else :
-    d = (b * b) - (4 * a * c)
+    d = float (b * b) - (4 * a * c)
     
 if d < 0 : 
     print("There are no roots in this equation because d < 0!")
 else :
-    x1 = (-b + (d ** (0.5))) / (2 * a)
-    x2 = (-b - (d ** (0.5))) / (2 * a)
+    x1 = float (-b + (d ** (0.5))) / (2 * a)
+    x2 = float (-b - (d ** (0.5))) / (2 * a)
 
 
 if (x1 != 0 and d >= 0) :
