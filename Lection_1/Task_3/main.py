2f83a3550dffdfe073a47dcbaef3d6d46aaf33f7
AntonZhiganov <Antohajigan@gmail.com>
2023-05-27 14:44:11 +0200

    Completed task 3 lectures 1

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -1,42 +1,42 @@
 import math
 
-firstNum = int  (input("Enter first number : "))       #Asking the user to enter 2 numbers
-secondNum = int  (input("Enter second number: "))
+firstNum = int(input("Enter the number 1: "))
+secondNum = int(input("Enter the number 2: "))
 
-print("1 - (+)")                #let the user choose an action
+print("1 - (+)")
 print("2 - (-)")
 print("3 - (*)")
 print("4 - (:)")
 print("5 - (exponentiation)")
-print("6 - (Square root)")
+print("6 - (square root)")
 
-action = int (input("What do you want to do with numbers: " ))
+action = int(input("What do you want to do with the numbers: "))
 
-print("===========================================")  
+print("===========================================")
 
-result = match action : 
-case 1 :                                          #The program reads the user's choice, 
-    "result of addition: " + (str(firstNum + secondNum))          #If the user enters a different number, then we inform him about it.
-case 2 :
-    "subtraction result: " + (str(firstNum - secondNum))
-case 3 :
-    "result of multiplication: " + (str(firstNum * secondNum))
-case 4 :
-    "result of division: " + (str(firstNum / secondNum))
-case 5 :
-    "result of exponentiation: " + (str(math.pow(firstNum, secondNum)))
-case 6 :
-    "What number do you want to extract the square root of? "
-    "Enter 1 if you want to take the square root of first number "
-    "Enter 2 if you want to take the square root of second number "
-    
-    choice = int (input("Do you want to take the square root of first number or secons number?: "))
+if action == 1:
+    result = "Addition result: " + str(firstNum + secondNum)
+elif action == 2:
+    result = "subtraction result: " + str(firstNum - secondNum)
+elif action == 3:
+    result = "Multiplication result: " + str(firstNum * secondNum)
+elif action == 4:
+    result = "division result: " + str(firstNum / secondNum)
+elif action == 5:
+    result = "The result of exponentiation: " + str(math.pow(firstNum, secondNum))
+elif action == 6:
+    print("What number do you want to take the square root of?")
+    print("Enter 1 if you want to take the square root of the first number")
+    print("Enter 2 if you want to take the square root of the second number")
+    choice = int(input("Choose a number to take the square root: "))
 
-root = match choice :
-    case 1 :
-        "The result of extracting the root from the number" + (str(firstNum)) + "is: " + (str(math.sqrt(firstNum)))
-    case 2 :
-        "The result of extracting the root from the number" + (str(secondNum)) + "is: " + (str(math.sqrt(secondNum)))
-        
-case _ :
-    "Enter numbers from 1 to 6!"
+    if choice == 1:
+        result = "The result of taking the square root of a number " + str(firstNum) + " equals: " + str(math.sqrt(firstNum))
+    elif choice == 2:
+        result = "The result of taking the square root of a number " + str(secondNum) + " equals: " + str(math.sqrt(secondNum))
+    else:
+        result = "Enter a number from 1 to 2!"
+else:
+    result = "Enter a number from 1 to 6!"
+
+print(result)
--- Lection_1/Task_4/main.py
+++ Lection_1/Task_4/main.py
@@ -1,28 +1,29 @@
+import match
 print("a - First element, b - second element, c - third element")  # Information for user
 
-a = float (input("enter a (a can't be 0): "))
-b = float (input("enter b : "))
-c = float (input("enter c : "))
+a = float(input("enter a (a can't be 0): "))
+b = float(input("enter b: "))
+c = float(input("enter c: "))
 
-if a == 0 :                                    # Checking the first element
-    print(" Error - a can't be 0")
-else :
-    d = float (b * b) - (4 * a * c)
-    
-if d < 0 :                                                        # Discriminant check
-    print("There are no roots in this equation because d < 0!")
-else :
-    x1 = float (-b + (d ** (0.5))) / (2 * a)
-    x2 = float (-b - (d ** (0.5))) / (2 * a)
+result = match(a)
+with result as m:
+    m.case(a == 0)
+    print("Error - a can't be 0")
+    m.case()
+    d = float(b * b) - (4 * a * c)
 
+if result:
+    with result as m:
+        m.case(d < 0)
+        print("There are no roots in this equation because d < 0!")
+        m.case()
+        x1 = (-b + (d ** 0.5)) / (2 * a)
+        x2 = (-b - (d ** 0.5)) / (2 * a)
 
-if (x1 != 0 and d >= 0) :                                     # Roots check
-    print("The first root of your equation : " + str(x1))
-else :
-    print("There is no first root of your equation!")
-if (x2 != 0 and d >= 0) :
-    print("The second root of your equation : " + str(x2))
-else :
-    print("There is no second root of your equation!")
-  
-    
+        with match(x1, x2) as m2:
+            m2.case((x1 != 0 and d >= 0), lambda: print(f"The first root of your equation: {x1}"))
+            m2.case()
+            print("There is no first root of your equation!")
+            m2.case((x2 != 0 and d >= 0), lambda: print(f"The second root of your equation: {x2}"))
+            m2.case()
+            print("There is no second root of your equation!")
