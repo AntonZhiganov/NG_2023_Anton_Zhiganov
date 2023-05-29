662daa041364156ddf839024febf72a976a011b4
AntonZhiganov <Antohajigan@gmail.com>
2023-05-29 17:35:41 +0200

    Now the library in the fourth task of the second lecture does not use jason, but uses a switch

--- Lection_2/Task_4/main.py
+++ Lection_2/Task_4/main.py
@@ -1,95 +1,85 @@
-import json
+def save_library(library):
+    with open('library.txt', 'w') as f:
+        for key, book in library.items():
+            f.write(f"{key}: {book}\n")
 
-def save_library (library) :                   # Save user books in library
-    with open ('library.txt', 'w') as f :
-        json.dump(library, f)
-
-def delete_Book (library) :                                            # Delete user books from library
-    name = input("Enter the name of the book you want to delete : ")
-    name = name
-    if name in library :                                              # Check if there is a book with the same name
+def delete_book(library):
+    name = input("Enter the name of the book you want to delete: ")
+    if name in library:
         del library[name]
-        print("The book has been delete!")
+        print("The book has been deleted!")
         save_library(library)
-    else :
+    else:
         print("The book was not deleted because it was not found!")
 
-# In the next part of the code, we ask the user for the value of the key, 
-# and also for the entire value of the key, and save the workbook to a word.
-    
-def add_Book(library) :                                                # Add user books in library
-    while True :
-        key = input("Enter the name of the book you want to add : ")     
-        nested_dic = {}
-        name = input("Enter book title : ")
-        author = input("Enter the author's full name : ")
-        pages = input("Enter number of pages : ")
-        genre = input("Enter the genre of the book : ")
-        cover = input("Enter book cover : ")
-        nested_dic ['name'] = name
-        nested_dic ['author'] = author
-        nested_dic ['pages'] = pages
-        nested_dic ['genre'] = genre
-        nested_dic ['cover'] = cover
-        library[key] = nested_dic
-        print("Your book has been added! ")
-        while True :
-            exit = input("Do you want to add another book? (yes/no)")
-            if exit == "no" :
-                return library
-            elif exit == "yes" :
+def add_book(library):
+    while True:
+        key = input("Enter the name of the book you want to add: ")
+        nested_dict = {}
+        name = input("Enter book title: ")
+        author = input("Enter the author's full name: ")
+        pages = input("Enter number of pages: ")
+        genre = input("Enter the genre of the book: ")
+        cover = input("Enter book cover: ")
+        nested_dict['name'] = name
+        nested_dict['author'] = author
+        nested_dict['pages'] = pages
+        nested_dict['genre'] = genre
+        nested_dict['cover'] = cover
+        library[key] = nested_dict
+        print("Your book has been added!")
+        while True:
+            exit_choice = input("Do you want to add another book? (yes/no)")
+            if exit_choice == "no":
+                return
+            elif exit_choice == "yes":
                 break
-            else :
+            else:
                 print("Enter yes or no!")
                 continue
-                
-def lookToBook(library) :                           # Looking for a book in the user's library
-    if not library :
+
+def look_up_book(library):
+    if not library:
         print("Library not found! First add a book!!")
-    else :
-        print("Looking for a library\n")
-        for key, book in library.items() :
-            print(f"{key} : {book}")
-                
-def edit_Book(library) :                                       # Edit a book in user's library
-    if not library :
+    else:
+        print("Looking up the library\n")
+        for key, book in library.items():
+            print(f"{key}: {book}")
+
+def edit_book(library):
+    if not library:
         print("Library not found! First add a book!!")
-    else :
-        name = input("Enter the title of the book you want to edit : ")      # We ask the user which of
-        if name in library :                                                 # his books he wants to edit
+    else:
+        name = input("Enter the title of the book you want to edit: ")
+        if name in library:
             book = library[name]
             print(f"Current details for {name}: {book}")
-            field = input("What value do you want to edit? (title, author, pages, genre, cover):")
-            if field in book :
-                newValue = input(f"Enter new value for {field}: ")
-                book[field] = newValue
-                library[name] = book
+            field = input("What value do you want to edit? (title, author, pages, genre, cover): ")
+            if field in book:
+                new_value = input(f"Enter new value for {field}: ")
+                book[field] = new_value
                 save_library(library)
-                print(f"{field} has been apdated for {name}")
-            else :
-                print(f"{field} is not to valid field for {name}")
-        else :
-            print(f"{name} is not faund in the library")
-try:                                                  # Avoiding Mistakes
-    with open('library.txt', 'r') as f :            
-        library = json.load(f)
-except FileNotFoundError :
-    library = {}
-    
-while True :                                   # Menu
-    menu = input("Enter 1 to add a book, 2 to delete a book, 3 to save and exit, \
-                 4 to wiev your books, 5 to edit book information : ")
-    if menu == "1" :
-        add_Book(library)
-    elif menu == "2" :
-        delete_Book(library)
-    elif  menu == "3" :
+                print(f"{field} has been updated for {name}")
+            else:
+                print(f"{field} is not a valid field for {name}")
+        else:
+            print(f"{name} is not found in the library")
+
+library = {}
+
+while True:
+    menu = input("Enter 1 to add a book, 2 to delete a book, 3 to save and exit, 4 to view your books, 5 to edit book information: ")
+    if menu == "1":
+        add_book(library)
+    elif menu == "2":
+        delete_book(library)
+    elif menu == "3":
         save_library(library)
         print("Changes saved!")
         break
-    elif menu == "4" :
-        lookToBook(library)
-    elif menu == "5" :
-        edit_Book(library)
-    else :
-        print("Please enter 1, 2, 4 or 5! ")
+    elif menu == "4":
+        look_up_book(library)
+    elif menu == "5":
+        edit_book(library)
+    else:
+        print("Please enter 1, 2, 4, or 5!")
