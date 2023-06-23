a717c4ecf7c1cd098a5a344b29bec85479366e03
AntonZhiganov <Antohajigan@gmail.com>
2023-06-24 01:46:04 +0200

    1) Added the ability to enter secrets 2) Now any user can log out of the account by clicking on the button

--- Project/app.py
+++ Project/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, url_for, request, flash, redirect
+from flask import Flask, render_template, url_for, request, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 import hashlib
+from flask_socketio import SocketIO, join_room, leave_room, emit
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = '11nbbn3m22nodp!2kfmd32'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+socketio = SocketIO(app)
 
 # Associating the LoginManager with our code
 login_manager = LoginManager()
@@ -20,18 +23,17 @@ class User(db.Model):
     
     # Add is_active attribute
     is_active  = db.Column(db.Boolean, default = True)
-    is_admin = db.Column(db.Boolean, default = True)
+    is_admin = db.Column(db.Boolean, default = False)
  
     def is_authenticated(self):
         return True
     
-    def is_anonimous(self):
+    def is_anonymous(self):
         return False
     
     def get_id(self):
         return(str(self.id))
-    
-     
+
     
 # Create a callback function decorator
 @login_manager.user_loader
@@ -49,7 +51,9 @@ class Chat(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(120), nullable = False)
     owner_id = db.Column(db.Integer, nullable = False)
+    secret = db.Column(db.String(50))
     messages = db.relationship('Message', backref = 'chat', lazy = True)
+    secret_entered = db.Column(db.Boolean, default=False)
     
 # MD5 encryption
 def encrypt_data(data):
@@ -98,6 +102,8 @@ def registration ():
 @app.route('/login', methods = ['POST', 'GET'])
 
 def login ():
+    if current_user.is_authenticated:
+        return redirect(url_for('chats'))
     
     if request.method == 'POST':
         username = request.form['username']
@@ -132,46 +138,113 @@ def chats ():
 def add_chat ():
     if request.method == 'POST':
         chat_name = request.form['chat_name']
+        secret = request.form['secret']
         owner_id = current_user.id
-        new_chat = Chat(name = chat_name, owner_id = owner_id)
+        if secret:
+            new_chat = Chat(name = chat_name, owner_id = owner_id, secret = secret)
+        else:
+            new_chat = Chat(name = chat_name, owner_id = owner_id)
         db.session.add(new_chat)
         db.session.commit()
-        
         flash('New chat added successfully', 'success')
         return redirect(url_for('chats'))
      
 # Delete chat                
-@app.route ('/delete_chat/<int:chat_id>', methods = ['POST'])
+@app.route('/delete_chat/<int:chat_id>', methods=['GET', 'POST'])
 @login_required
 def delete_chat(chat_id):
-    chat = Chat.query.get_or_404(chat_id)
-    
-    # Check if the current user is the chat owner or administrator
-    if current_user.id == chat.owner_id or current_user.is_admin:
-        db.session.delete(chat)
-        db.session.commit()
-        flash('chat deleted successfully', 'success')
-    else:
-        flash('You do not have permission to delete this chat', 'error')
-
+    chat = Chat.query.get(chat_id)
+    if chat.owner_id != current_user.id:
+        flash('У вас нет прав на удаление этого чата', 'danger')
+        return redirect(url_for('chats'))
+    if chat.messages:
+        for message in chat.messages:
+            db.session.delete(message)
+    db.session.delete(chat)
+    db.session.commit()
+    flash('Чат успешно удален', 'success')
     return redirect(url_for('chats'))
         
+# Open chat
 @app.route ('/chat/<int:chat_id>', methods = ['GET', 'POST'])
 @login_required
 def open_chat(chat_id):
     chat = Chat.query.get_or_404(chat_id)
+    if chat.secret:
+        if not chat.secret_entered:
+            return redirect(url_for('chat_secret', chat_id = chat.id))
+        else:
+            if request.method == 'POST':
+                content = request.form.get('content')
+                if content:
+                    message = Message(content = content, chat_id = chat_id)
+                    db.session.add(message)
+                    db.session.commit()
+                    socketio.emit('new_message',{'content': content}, room = chat_id)
+                return redirect(url_for('open_chat', chat_id = chat.id))
+            else:
+                messages = Message.query.filter_by(chat_id = chat_id).all()
+                return render_template('chat.html', chat = chat, messages = messages)       
+    else:
+        if request.method == 'POST':
+            content = request.form.get('content')
+            if content:
+                message = Message(content = content, chat_id = chat_id)
+                db.session.add(message)
+                db.session.commit()
+                socketio.emit('new_message',{'content': content}, room = chat_id)
+            return redirect(url_for('open_chat', chat_id = chat.id))
+        else:
+            messages = Message.query.filter_by(chat_id = chat_id).all()
+            return render_template('chat.html', chat = chat, messages = messages)
+ 
+# User authorization in the chat
+def login_user_chat(chat):
+    session['current_user'] = str(current_user.id)
+    active_users = session.get('active_users', [])
+    if str(current_user.id) not in active_users:
+        active_users.append(str(current_user.id))
+    session['active_users'] = active_users
+    session['authenticated_chat_id'] = str(chat.id)     
+
+# Chat secret
+@app.route('/chat/<int:chat_id>/secret', methods=['GET', 'POST'])
+@login_required
+def chat_secret (chat_id):
+    chat = Chat.query.get_or_404(chat_id)
     if request.method == 'POST':
-        content = request.form.get('content')
-        if content:
-            message = Message(content = content, chat_id = chat_id)
-            db.session.add(message)
+        secret = request.form.get('secret')
+        if secret == chat.secret:
+            chat.secret_entered = True
             db.session.commit()
-    messages = Message.query.filter_by(chat_id = chat_id).all()
-    return render_template('chat.html', chat = chat, messages = messages)
+            return redirect(url_for('open_chat', chat_id = chat.id))
+        else:
+            flash('Wront secret', 'error')
+    return render_template('chat_secret.html', chat=chat)
+
+# join user in chat
+@socketio.on('join')
+def on_join(data):
+    chat_id = data['chat_id']         
+    join_room(chat_id)
+    emit('status', {'msg': 'You have entered the chat'}, room=chat_id) 
+    
+# Leave user from chat
+@socketio.on('leave')
+def on_leave(data):
+    chat_id = data['chat_id']
+    leave_room(chat_id)
+    emit('status', {'msg': 'You have left the chat'}, room=chat_id)
+
+# logout
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
         
 # Start server
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug = True)
-    
+    socketio.run(app, debug = True)
