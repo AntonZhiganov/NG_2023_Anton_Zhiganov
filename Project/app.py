acc674151695a4e478f3fee04423e4e1c852c8fe
AntonZhiganov <Antohajigan@gmail.com>
2023-06-27 20:32:07 +0200

    add port 3030

--- Project/app.py
+++ Project/app.py
@@ -229,4 +229,4 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', port=80, debug = True)
+    socketio.run(app, port = 3030, debug = True)
